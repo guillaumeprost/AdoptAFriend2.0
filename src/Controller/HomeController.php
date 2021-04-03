8260281ceff04dac97cc526ea5c7983dc4322247
Guillaume Prost <guillaume.prost@abbeal.com>
2021-04-03 21:44:28 +0200

    install webpack encore

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guillaumeprost
+ * Date: 06/05/2016
+ * Time: 18:05
+ */
+
+namespace App\Controller;
+
+use AppBundle\Entity\Offer;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class HomeController
+ * @package AppBundle\Controller
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('home/index.html.twig', [
+        ]);
+    }
+}
