7c9bea8516d4ae6033996d8a1c3c40c5994808d4
Guillaume Prost <guillaume.prost@abbeal.com>
2021-05-19 21:28:49 +0200

    add dashboard view

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,10 +1,21 @@
 <?php
 
-
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
-class UserController
+class UserController extends AbstractController
 {
-
+    /**
+     * @Route("/dashboard", name="user_dashboard")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function dashboard(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('user/dashboard.html.twig');
+    }
 }
